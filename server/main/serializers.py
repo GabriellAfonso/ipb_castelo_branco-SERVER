1dbfe56d470f36ebf59f0e4f2a933964b754b731
Gabriel Afonso <gabrieldelimaafonso@gmail.com>
2026-01-31 23:16:38 -0300

    Feat: Add RegisterSerializer for user registration with password validation

--- server/main/serializers.py
+++ server/main/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from main.models.songs import Song, Played
+from django.contrib.auth.models import User
 
 
 class SongSerializer(serializers.ModelSerializer):
@@ -15,3 +16,22 @@ class PlayedSerializer(serializers.ModelSerializer):
     class Meta:
         model = Played
         fields = ['id', 'song', 'date', 'tone', 'position']
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=6)
+    password_confirm = serializers.CharField(write_only=True, min_length=6)
+
+    class Meta:
+        model = User
+        fields = ["username", "password", "password_confirm"]
+
+    def validate(self, data):
+        if data["password"] != data["password_confirm"]:
+            raise serializers.ValidationError(
+                {"password_confirm": "Passwords do not match."})
+        return data
+
+    def create(self, validated_data):
+        validated_data.pop("password_confirm")
+        return User.objects.create_user(**validated_data)
