6b115e32a87bbaa4df255245e9a9358de6259717
Gabriel Afonso <gabrieldelimaafonso@gmail.com>
2026-01-31 23:16:30 -0300

    Fix: Correct import order in admin.py for better readability

--- server/main/admin.py
+++ server/main/admin.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
 from main.models.songs import Category, Song, Played
-from main.models.member import Member
+from main.models.profile import Profile
 from main.models.schedule import ScheduleType, MemberScheduleConfig, MonthlySchedule
 from main.models.hymnal import Hymn
-
+from main.models.member import Member
 admin.site.register(Category)
 admin.site.register(Song)
 admin.site.register(Played)
 
 admin.site.register(Member)
+admin.site.register(Profile)
 admin.site.register(ScheduleType)
 admin.site.register(MemberScheduleConfig)
 admin.site.register(MonthlySchedule)
