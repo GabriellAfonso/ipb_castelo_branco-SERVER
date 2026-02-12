d03d837bec4d0b25384779c86a7bbbc95d91c5a9
Gabriel Afonso <gabrieldelimaafonso@gmail.com>
2026-02-12 16:53:44 -0300

    feat(serializers): add RegisterSerializer for user registration

---
+++ server/apps/api/serializers/register_serializer.py
@@ -0,0 +1,64 @@
+from typing import Any
+from rest_framework import serializers
+from core.application.dtos.auth_dtos import RegisterDTO
+from rest_framework import serializers
+from django.utils.translation import gettext_lazy as _
+
+
+class RegisterSerializer(serializers.Serializer):
+    username = serializers.CharField(
+        max_length=150,
+        error_messages={
+            "blank": _("Este campo não pode ficar em branco."),
+            "required": _("Este campo é obrigatório."),
+        },
+    )
+    first_name = serializers.CharField(
+        max_length=30,
+        error_messages={
+            "blank": _("Este campo não pode ficar em branco."),
+            "required": _("Este campo é obrigatório."),
+        },
+    )
+    last_name = serializers.CharField(
+        max_length=150,
+        error_messages={
+            "blank": _("Este campo não pode ficar em branco."),
+            "required": _("Este campo é obrigatório."),
+        },
+    )
+    password = serializers.CharField(
+        write_only=True,
+        min_length=6,
+        error_messages={
+            "min_length": _("A senha precisa ter ao menos 6 caracteres."),
+            "blank": _("Este campo não pode ficar em branco."),
+            "required": _("Este campo é obrigatório."),
+        },
+    )
+    password_confirm = serializers.CharField(
+        write_only=True,
+        required=True,
+        allow_blank=True,
+        min_length=6,
+        error_messages={
+            "min_length": _("A senha precisa ter ao menos 6 caracteres."),
+            "blank": _("Este campo não pode ficar em branco."),
+            "required": _("Este campo é obrigatório."),
+        },
+    )
+
+    def validate(self, data: dict[str, Any]):
+        if data.get("password") != data.get("password_confirm"):
+            raise serializers.ValidationError({
+                "password_confirm": [_("As senhas não coincidem.")]
+            })
+        return data
+
+    def create_dto(self):
+        return RegisterDTO(
+            username=self.validated_data.get("username"),
+            password=self.validated_data.get("password"),
+            first_name=self.validated_data.get("first_name"),
+            last_name=self.validated_data.get("last_name"),
+        )
