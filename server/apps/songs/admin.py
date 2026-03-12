056f38dad93786853813eb9db5c54ad08b9aaa0a
GabrielAfonso <gabriellafonso.dev@gmail.com>
2026-03-12 14:10:27 -0700

    chore(songs): register ChordChart and Lyrics in admin

--- server/apps/songs/admin.py
+++ server/apps/songs/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
+from apps.songs.models.chord_chart import ChordChart
 from apps.songs.models.hymnal import Hymn
+from apps.songs.models.lyrics import Lyrics
 from apps.songs.models.song import Category, Played, Song
 
-admin.site.register([Category, Song, Played, Hymn])
+admin.site.register([Category, Song, Played, Hymn, ChordChart, Lyrics])
